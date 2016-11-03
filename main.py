a835e842334d46c9864e22aabfda90611eb7075e
Khairul Imam <ki6559@gmail.com>
2016-11-04 06:21:33 +0800

    delete unused folder add .idea to gitignore

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import numpy as np
 import pulp
-
+import seaborn as sns
+from matplotlib import pyplot as plt
+from matplotlib.patches import PathPatch
+from matplotlib.path import Path
 import solver
 
 bayam = 'bayam'
@@ -35,3 +39,40 @@ max_magnesium = 248 * x.value() + 243 * y.value()
 print "Max protein yang didapat sehari adalah {}g dari total minimal yang dibutuhkan (50g)".format(max_protein)
 print "Max magnesium yang didapat sehari adalah {}mg dari total minimal yang dibutuhkan (400mg)".format(max_magnesium)
 print "Dengan pengeluaran perhari {} dari 15000 jatah belanja perhari".format(2500 * x.value() + 2000 * y.value())
+
+# graphic
+sns.set_palette('Set1')
+
+# create the plot object
+fig, ax = plt.subplots(figsize=(8, 8))
+s = np.linspace(0, 10)
+
+plt.plot(s, 50 - 14 * s, lw=3, label='protein')
+plt.fill_between(s, 0, 50 - 14 * s, alpha=0.1)
+plt.plot(s, 400 - 248 * s, lw=3, label='magnesium')
+plt.fill_between(s, 0, 400 - 248 * s, alpha=0.1)
+plt.plot(s, 15000 - 2500 * s, lw=3, label='harga')
+plt.fill_between(s, 0, 15000 - 2500 * s, alpha=0.1)
+
+# add non-negativity constraints
+plt.plot(np.zeros_like(s), s, lw=3, label='tempe kuadran 1')
+plt.plot(s, np.zeros_like(s), lw=3, label='bayam kuadran 1')
+
+# highlight the feasible region
+path = Path([
+    (0., 2.),
+    (0., 1.),
+    (0., 7.),
+    (6., 0.),
+    (3., 0.),
+])
+patch = PathPatch(path, label='Wilayah Penyelesaian', alpha=0.5)
+ax.add_patch(patch)
+
+# labels and stuff
+plt.xlabel('bayam', fontsize=16)
+plt.ylabel('tempe', fontsize=16)
+plt.xlim(-0.5, 10)
+plt.ylim(-0.5, 10)
+plt.legend(fontsize=14)
+plt.show()
